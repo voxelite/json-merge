dec2d3c24e59dd7502198ec9b38bb1d3f0c6baae
Abit Gray <AbitTheGray@gmail.com>
2024-06-17 21:39:47 +0200

    Init

---
+++ index.js
@@ -0,0 +1,53 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fs = require('fs');
+
+const inFile = core.getInput('in-file');
+const inDirectory = core.getInput('in-directory');
+const outFile = core.getInput('out-file');
+
+try
+{
+    core.info("inFile: " + inFile);
+    core.info("inDirectory: " + inDirectory);
+    core.info("outFile: " + outFile);
+
+    let outputJson = JSON.constructor();
+
+    if(inFile !== undefined)
+    {
+        const fileData = fs.readFileSync(inFile, 'utf8');
+        outputJson = JSON.parse(fileData);
+    }
+
+    if(inDirectory !== undefined)
+    {
+        fs.readdirSync(inDirectory).forEach(filename => {
+            if(!filename.endsWith(".json"))
+                return;
+
+            const filenameWithoutExtension = filename.substring(0, filename.length - ".json".length);
+            if(outputJson[filenameWithoutExtension] !== undefined)
+            {
+                core.error(filenameWithoutExtension + " is already defined (in " + inFile + ")");
+                return;
+            }
+
+            const fileData = fs.readFileSync(inDirectory + '/' + filename, { encoding: 'utf8', flag: 'r' });
+            outputJson[filenameWithoutExtension] = JSON.parse(fileData);
+        });
+    }
+
+    {
+        fs.writeFileSync(
+            outFile,
+            JSON.stringify(outputJson, null, null),
+            { encoding: 'utf8', flag: 'w' }
+        );
+    }
+
+}
+catch(error)
+{
+    core.setFailed(error.message);
+}
